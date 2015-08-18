b083378dfff1adc7454e04b934492458f3f34cec
LukeHaines <lukehaines2@gmail.com>
2015-08-18 21:21:48 +0100

    ACTUALLY ADDED A JS SOURCE OMG SUICIDE

--- javascript.js
+++ javascript.js
@@ -11,16 +11,20 @@ var end = document.getElementById(endpoint)
 end.addEventListener('change', function(event){
   startIndex = Train.lineN.indexOf(start.value)
   endIndex = Train.lineN.indexOf(end.value)
-});
+  console.log(startIndex)
+  console.log(endIndex)
 
-var x = document.getElementById("mySelect").value;
-// var x = document.getElementById("myForm").elements.length;
 
 event.preventDefault;
 
-var result = Math.abs(lineN.indexOf(start)) - (lineN.indexOf(end));
+
+var result = Math.abs(startIndex - endIndex);
   console.log(result);
+});
+
+// var result = Math.abs(lineN.indexOf(start)) - (lineN.indexOf(end));
+//   console.log(result);
 
 //chnage inner html to equal the result
 
-.innerHTML = 
+// .innerHTML = 
