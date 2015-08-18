4d8c2dfad4a63e999f3f29e01eef285109525632
LukeHaines <lukehaines2@gmail.com>
2015-08-18 20:48:56 +0100

    adding event listener function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-Var Subway = {
+Var Train = {
  'lineN' = ["Times Square", "34th", "n-28th", 'n-23rd', 'n-Union Square', 'n-8th'],
  'lineL' = ["l-8th", "6th", "l-Union Square", "3rd", "1st"],
  'line6' = ['Grand Central', '33rd', '6-28th', 'n-23rd', '6-Union Square', 'Astor Place']
@@ -8,15 +8,15 @@ Var Subway = {
 var start = document.getElementById(startpoint);
 var end = document.getElementById(endpoint)
  
-end.addEventListener('change', function(e){
-  start
-  end
-}
+end.addEventListener('change', function(event){
+  startIndex = Train.lineN.indexOf(start.value)
+  endIndex = Train.lineN.indexOf(end.value)
+});
 
 var x = document.getElementById("mySelect").value;
 // var x = document.getElementById("myForm").elements.length;
 
-e.preventDefault;
+event.preventDefault;
 
 var result = Math.abs(lineN.indexOf(start)) - (lineN.indexOf(end));
   console.log(result);
