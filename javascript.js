82e1b34a0d6d5ff1f31252da44a9263d7c845579
LukeHaines <lukehaines2@gmail.com>
2015-08-18 20:17:46 +0100

    forgot to commit for a while

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+Var Subway = {
+ 'lineN' = ["Times Square", "34th", "n-28th", 'n-23rd', 'n-Union Square', 'n-8th'],
+ 'lineL' = ["l-8th", "6th", "l-Union Square", "3rd", "1st"],
+ 'line6' = ['Grand Central', '33rd', '6-28th', 'n-23rd', '6-Union Square', 'Astor Place']
+};
+
+var start = document.getElementById(startpoint);
+var end = document.getElementById(endpoint)
+ 
+end.addEventListener('change', function(e){
+  start
+  end
+}
+
+var x = document.getElementById("mySelect").value;
+// var x = document.getElementById("myForm").elements.length;
+
+e.preventDefault;
+
+var result = Math.abs(lineN.indexOf(start)) - (lineN.indexOf(end));
+  console.log(result);
+
+//chnage inner html to equal the result
+
+.innerHTML = 
